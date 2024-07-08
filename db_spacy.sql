c0b9ca1ab0cee17d7cab7507192c3917485559e3
Defri Indra Mahardika <defrindr@gmail.com>
2024-07-08 23:55:16 +0700

    update: db add new table

--- db_spacy.sql
+++ db_spacy.sql
@@ -7,6 +7,25 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
+DROP TABLE IF EXISTS `cleaner`;
+CREATE TABLE `cleaner` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `file` varchar(255) NOT NULL,
+  `result` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `crawler`;
+CREATE TABLE `crawler` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `keyword` varchar(255) NOT NULL,
+  `result` varchar(255) NOT NULL,
+  `limit` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
 DROP TABLE IF EXISTS `uploaders`;
 CREATE TABLE `uploaders` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -15,7 +34,7 @@ CREATE TABLE `uploaders` (
   `spacy` text NOT NULL,
   `result` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 DROP TABLE IF EXISTS `users`;
@@ -29,10 +48,10 @@ CREATE TABLE `users` (
   `flag` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO `users` (`id`, `username`, `password`, `role`, `name`, `photo`, `flag`) VALUES
 (1,	'admin',	'21232f297a57a5a743894a0e4a801fc3',	'ADMIN',	'Administrator',	'admin.png',	1),
 (2,	'defrindr',	'42c0402d1db4bbdcdb5aa62759e76522',	'ADMIN',	'defrindr',	'defrindr.png',	1);
 
--- 2024-07-03 16:26:48
+-- 2024-07-08 16:54:45
